fe0f50cd02eabd58268eb815ed6038f25dc17c2a
Daniil Karpov <karpov.daniil@bk.ru>
2025-04-15 00:20:41 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Endurance ЕГЭ",
+    pageTitle: "Endurance",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
